cbd4fecac67f1ebe1ab11d11c2ce3df489c3058d
mufaawan <mufaawan@gmail.com>
2017-12-26 01:56:23 +0500

    Cam and Recognition

---
+++ camtest.py
@@ -0,0 +1,15 @@
+import cv2
+
+# initialize the camera
+cam = cv2.VideoCapture(0)
+ret, image = cam.read()
+
+if ret:
+    cv2.imshow('SnapshotTest',image)
+    cv2.waitKey(0)
+    cv2.destroyWindow('SnapshotTest')
+    cv2.imwrite('SnapshotTest.jpg',image)
+else:
+  print ("No Camera Found")
+
+cam.release()
