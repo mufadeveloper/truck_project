d8df8006d94ce48df3b305a24d0a1636b7014dea
mufaawan <mufaawan@gmail.com>
2017-12-26 02:07:40 +0500

    Cam and Recognition

--- camtest.py
+++ camtest.py
@@ -5,7 +5,7 @@ cam = cv2.VideoCapture(0)
 ret, image = cam.read()
 
 if ret:
-    cv2.imshow('SnapshotTest',image)
+    #cv2.imshow('SnapshotTest',image)
     cv2.waitKey(0)
     cv2.destroyWindow('SnapshotTest')
     cv2.imwrite('SnapshotTest.jpg',image)
